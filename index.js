313a838df2d6099f35cdee94373800a43f2a9c82
+ <brandi.ude@gmail.com>
2021-10-13 17:13:44 -0400

    commit tree f9a8875db003c6086998639542b35304cc949175

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(depth){
+  if (depth <= 400) {
+    return 'This one is on me!';
+  } else if (2000 <= depth && depth < 2500){
+    return 'I will gladly take your thirty bucks.';
+  } else if (depth >= 2500) 
+    return 'No can do.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city = 'NYC'){
+ return city == 'NYC' ? 'Ok, sounds good.' : 'No go.';
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+       return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return "Bye.";
+  }
 }
